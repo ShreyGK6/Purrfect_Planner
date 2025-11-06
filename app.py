ec579618eebf476fe4f6c50caba020bf8527be5b
Shivani Nandakumar <snand021@ucr.edu>
2025-11-05 16:05:12 -0800

    added flash messages and links in navbar

--- app.py
+++ app.py
@@ -1,12 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
 
 
 app = Flask(__name__)
-app.secret_key = "dev-key" # for flash messages only
+app.secret_key = "dev-key" #for flash messages only
 
 @app.route("/")
 def home():
+    flash("home", "success")
     return render_template("base.html")
+
+@app.route("/login")
+def login():
+    flash("login", "success")
+    return render_template("base.html")
+
+@app.route("/add_task")
+def add_task():
+    flash("add_task", "success")
+    return render_template("base.html")
+
+
+@app.route("/register")
+def register():
+    flash("register", "success")
+    return render_template("base.html")
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
