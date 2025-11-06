ef463e734e6b8d60f1eafe69e2eec401a321eadf
Shivani Nandakumar <snand021@ucr.edu>
2025-11-05 16:28:22 -0800

    setting up login and register pages, login page tested and works, need login auth

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, flash
+from flask import Flask, render_template, flash, request, redirect, url_for
 
 
 app = Flask(__name__)
@@ -9,10 +9,23 @@ def home():
     flash("home", "success")
     return render_template("base.html")
 
-@app.route("/login")
+@app.route("/login", methods = ['POST', 'GET'])
 def login():
-    flash("login", "success")
-    return render_template("base.html")
+    if request.method == 'POST':
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+
+        #need password authentication here (KEERTHIS PART)
+        if username and password:
+            flash(f"Welcome back, {username}!", 'success')
+            return redirect(url_for("home"))
+        else:
+            flash("Invalid login.", "danger")
+            return redirect(url_for("login.html"))
+
+
+    return render_template("login.html")
 
 @app.route("/add_task")
 def add_task():
